842b4e61ae7891d0001c972ee0b0ace24a5a8cda
Dipika Sikka <dipikasikka1@gmail.com>
2019-07-16 20:59:28 -0400

    Remove tutorials

--- Tutorial 2/tutorial_2_notes.py
+++
@@ -1,4 +0,0 @@
-# Centre the data by subtracting the mean to each data point
-# Use dot product and project onto each unit vector --> determine which gives the highest variance to find a principal component
-# TWo dimentionsal data produces two dimenions, from ehich you choose to reduce 
-# Transforming the data by simply rotating does not produce any error
